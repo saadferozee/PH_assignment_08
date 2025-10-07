cfa52633a3133ab23df05f75a443450c0a5a404a
saadferozee <saadferozee@gmail.com>
2025-10-08 01:13:07 +0600

    Live site deploy check

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import './App.css'
 function App() {
 
   return (
-    <>
-      {/* Program Start Now --> 12:39:34 AM, Wed 8 Oct 2025 */}
-    </>
+    <div className='font-inter'>
+      <h1>Program Start Now -- 12:39:34 AM, Wed 8 Oct 2025</h1>
+    </div>
   )
 }
 
